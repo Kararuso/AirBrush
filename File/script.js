ecf08c7e1433213d363caed58c9e32b649549b29
Kararuso <kirill.shchuchkin@gmail.com>
2021-02-18 22:44:18 +0300

    Add project AirBrush

---
+++ File/script.js
@@ -0,0 +1,14 @@
+const anchors = document.querySelectorAll('.article_button a[href*="#search"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+  e.preventDefault()
+   
+  const blockID = anchor.getAttribute('href').substr(1)
+   
+  document.getElementById(blockID).scrollIntoView({
+  behavior: 'smooth',
+  block: 'start'
+  })
+  })
+}
